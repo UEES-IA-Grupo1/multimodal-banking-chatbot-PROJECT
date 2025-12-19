b55782a3b1591e650b288081ea3bc73fec866d40
Ricardo Campoverde <ricardo.campoverde@uees.edu.ec>
2025-12-18 23:47:01 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='proyecto-ia-banca',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[], # Definidos en requirements.txt
+)
